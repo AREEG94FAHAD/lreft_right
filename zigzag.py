a00bf14a3bf85f200d3c425375de42d403a30b83
AREEG94FAHAD <30151596+AREEG94FAHAD@users.noreply.github.com>
2022-10-02 10:18:23 +0300

    Update zigzag.py

--- zigzag.py
+++ zigzag.py
@@ -1,14 +1,16 @@
 def solution(a):
     b = []
-    for i in range(len(a)//2):
+    len_a = len(a)
+
+    for i in range(len_a//2):
         b.append(a[i])
         b.append(a[-1-i])
         
-    if len(b) != len(a):
-        b.append(a[math.ceil(len(a)//2)])
+    if len(b) != len_a:
+        b.append(a[math.ceil(len_a//2)])
         
-    print(b)
-    if len(set(b)) != len(b):
+    len_b = len(b)
+    if len(set(b)) != len_b:
         return False
     return b == sorted(a)
 
